60819a8bdc086f013ca20c72a29a05c281479601
Priyal0711 <priyal.chaudhari@brainvire.com>
2023-09-08 11:34:09 +0530

    Creating Restful Api

---
+++ app/Http/Resources/EventResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Event;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EventResource extends JsonResource
+{
+    public function index()
+    {
+        return EventResource::collection(Event::all());
+    }
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return parent::toArray($request);
+    }
+}
